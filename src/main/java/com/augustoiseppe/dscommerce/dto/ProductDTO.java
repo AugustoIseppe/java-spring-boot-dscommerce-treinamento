43aaa1419ad4fdd466a7e0407e847fea218a7ca4
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-04-02 17:14:09 -0300

    Primeiro commit do Projeto DSCommerce

---
+++ src/main/java/com/augustoiseppe/dscommerce/dto/ProductDTO.java
@@ -0,0 +1,58 @@
+package com.augustoiseppe.dscommerce.dto;
+
+import com.augustoiseppe.dscommerce.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+public class ProductDTO {
+    private Long id;
+    @NotBlank(message = "Campo requerido")
+    @Size(min = 3, max = 80, message = "O nome precisa ter de 3 a 8 caracteres")
+    private String name;
+    @NotBlank(message = "Campo requerido")
+    @Size(min = 10, message = "A descrição precisa ter no mínimo 10 caracteres")
+    private String description;
+    @Positive(message = "O preço deve ser positivo")
+    private double price;
+    private String imgUrl;
+
+    public ProductDTO () {}
+
+    public ProductDTO(Long id, String name, String description, double price, String imgUrl) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgUrl = imgUrl;
+    }
+
+    public ProductDTO(Product entity) {
+        id = entity.getId();
+        name = entity.getName();
+        description = entity.getDescription();
+        price = entity.getPrice();
+        imgUrl = entity.getImgUrl();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+}
