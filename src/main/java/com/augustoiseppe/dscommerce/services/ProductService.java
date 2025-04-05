a8487c67cd49f291f835ae3e9342d401f02b01c6
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-04-05 11:09:18 -0300

    ProductMinDTO

--- src/main/java/com/augustoiseppe/dscommerce/services/ProductService.java
+++ src/main/java/com/augustoiseppe/dscommerce/services/ProductService.java
@@ -1,6 +1,7 @@
 package com.augustoiseppe.dscommerce.services;
 
 import com.augustoiseppe.dscommerce.dto.ProductDTO;
+import com.augustoiseppe.dscommerce.dto.ProductMinDTO;
 import com.augustoiseppe.dscommerce.entities.Product;
 import com.augustoiseppe.dscommerce.repositories.ProductRepository;
 import com.augustoiseppe.dscommerce.services.exceptions.DatabaseException;
@@ -32,9 +33,9 @@ public class ProductService {
     }
 
     @Transactional(readOnly = true)
-    public Page<ProductDTO> findAll(String name, Pageable pageable) {
+    public Page<ProductMinDTO> findAll(String name, Pageable pageable) {
         Page<Product> result = repository.searchByName(name, pageable);
-        return result.map(x -> new ProductDTO(x));
+        return result.map(x -> new ProductMinDTO(x));
     }
 
     @Transactional()
