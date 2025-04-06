1a5f5a3af33f9a55c4a0aa48aac67ee8d78b2d82
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-04-06 14:25:22 -0300

    get categories

---
+++ src/main/java/com/augustoiseppe/dscommerce/controllers/CategoryController.java
@@ -0,0 +1,23 @@
+package com.augustoiseppe.dscommerce.controllers;
+
+import com.augustoiseppe.dscommerce.dto.CategoryDTO;
+import com.augustoiseppe.dscommerce.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> list = categoryService.findAll();
+        return ResponseEntity.ok(list);
+    }
+}
---
+++ src/main/java/com/augustoiseppe/dscommerce/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.augustoiseppe.dscommerce.repositories;
+
+import com.augustoiseppe.dscommerce.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+
+
+}
